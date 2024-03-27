ea2a792cc881f5254bb87d7f589793f83ac1a01b
Mohamed Magdy <mohamed.eldoghry@contact.eg>
2024-03-27 18:28:25 +0200

    Revert "handling errors"
    This reverts commit a11f87d4b121e4a5de02dc8ed54150b8e662ccb2.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import dbConnect from "./config/db";
+import authRouter from "./routes/auth.route";
+import {
+  errorHandler,
+  urlNotFound,
+} from "./middlewares/error-handler.middleware";
+
 dotenv.config();
 
-import appSetup from "./startup/init";
-import securitySetup from "./startup/secuirty";
-import routerSetup from "./startup/router";
-
 const app = express();
+const PORT = process.env.PORT || 4000;
 
-appSetup(app);
-securitySetup(app, express);
-routerSetup(app);
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("short"));
+
+dbConnect();
+
+app.use("/api/auth", authRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).send("hello world");
+});
+
+// error handler middleware
+app.all("*", urlNotFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log("Server listening on port " + PORT);
+});
