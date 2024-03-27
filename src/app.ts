c7e3c135b7f7ee21a6ced0c23f0c05e7ee69c6dc
Mohamed Magdy <mohamed.eldoghry@contact.eg>
2024-03-27 18:15:24 +0200

    Revert "basic setup"
    This reverts commit e32133eeb8085fc22b86eb6ca147968e2b6c2721.

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-import morgan from "morgan";
-import dbConnect from "./config/db";
-import authRouter from "./routes/auth.route";
-
 dotenv.config();
 
+import appSetup from "./startup/init";
+import securitySetup from "./startup/secuirty";
+import routerSetup from "./startup/router";
+
 const app = express();
-const PORT = process.env.PORT || 4000;
 
-// middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(morgan("short"));
-
-dbConnect();
-
-app.use("/api/auth", authRouter);
-
-app.get("/", (req, res) => {
-  res.status(200).send("hello world");
-});
-
-app.listen(PORT, () => {
-  console.log("Server listening on port " + PORT);
-});
+appSetup(app);
+securitySetup(app, express);
+routerSetup(app);
