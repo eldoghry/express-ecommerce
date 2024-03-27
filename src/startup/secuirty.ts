e32133eeb8085fc22b86eb6ca147968e2b6c2721
Mohamed Magdy <mohamed.eldoghry@contact.eg>
2024-03-27 17:10:07 +0200

    basic setup

--- src/startup/secuirty.ts
+++
@@ -1,7 +0,0 @@
-import { Express } from "express";
-import cors from "cors";
-
-// store any security related things
-const securitySetup = (app: Express, express: any) =>
-  app.use(cors()).use(express.json());
-export default securitySetup;
