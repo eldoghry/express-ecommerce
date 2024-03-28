3d6fcc91028a0dee07cc8f357c063acd8e6834a7
Mohamed Magdy <mohamed.eldoghry@contact.eg>
2024-03-28 10:51:16 +0200

    user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,23 @@
+import User, { IUser } from "../models/user.model";
+
+const create = async (data: { [key: string]: any }) => {
+  return User.create(data);
+};
+
+const findOne = async (filter: { [key: string]: any }) => {
+  return User.findOne(filter);
+};
+
+const findAll = async (filter: { [key: string]: any } = {}) => {
+  return User.find(filter);
+};
+
+const updateOne = async (data: Partial<IUser>) => {
+  return User.updateOne();
+};
+
+const deleteOne = async (_id: string) => {
+  return (await User.deleteOne({ _id })).deletedCount === 1;
+};
+
+export { create, findOne, findAll, updateOne, deleteOne };
