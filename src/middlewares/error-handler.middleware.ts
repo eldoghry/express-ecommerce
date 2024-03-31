a7346bcfd57b88787cd7d3cbc710f3e9c210c45b
Mohamed Magdy <mohamed.eldoghry@contact.eg>
2024-03-31 15:56:00 +0200

    validate dtos and create global handler

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,24 +1,18 @@
+import { ClassConstructor, plainToClass } from "class-transformer";
+import { validate } from "class-validator";
 import { NextFunction, Request, Response } from "express";
 import createError, { HttpError } from "http-errors";
 
-export const urlNotFound = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const urlNotFound = (req: Request, res: Response, next: NextFunction) => {
   next(createError(404, `Route not found`));
 };
 
-export const errorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
 
   const isDev = process.env.NODE_ENV?.trim() !== "production";
 
+  console.log(err, typeof err);
   if (err instanceof HttpError) statusCode = err.statusCode;
 
   res.status(statusCode).json({
@@ -27,4 +21,16 @@ export const errorHandler = (
     message: err.message,
     stack: isDev ? err?.stack : undefined,
   });
+};
+
+export const validateDtoMiddleware = function <T>(dtoClass: ClassConstructor<T>) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const dtoInstance = plainToClass(dtoClass, req.body);
+    const errors = await validate(dtoInstance as object);
+    if (errors.length > 0) {
+      const messages = errors.map((err) => Object.values(err.constraints!)).join(", ");
+      next(createError(400, messages));
+    }
+    next();
+  };
 };
